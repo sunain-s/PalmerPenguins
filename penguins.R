a0b1709f8ddfb144e8d3f1e47b554ca15c59d225
sunain-s <Sunain.Syed@communities.gov.uk>
2026-06-24 14:50:23 +0100

    feat(penguins): plot avg mass by species (bar chart)

--- penguins.R
+++ penguins.R
@@ -1,13 +1,35 @@
+# install.packages("tidyverse")
+# install.packages("palmerpenguins")
+
 library(tidyverse)
 library(palmerpenguins)
 
-penguins <- penguins
 head(penguins)
 summary(penguins)
 dim(penguins)
 names(penguins)
 str(penguins)
-View(penguins)
+
+
+# Heaviest Species
+avg_mass_by_species <- penguins %>%
+  group_by(species) %>%
+  summarise(avg_mass = mean(body_mass_g, na.rm=TRUE)) %>%
+  arrange(desc(avg_mass))
+avg_mass_by_species
+
+ggplot(avg_mass_by_species,
+       aes(x = species, y = avg_mass, fill = species)) + 
+  geom_col() +
+  labs(title = "Average Body Mass by Penguin Species",
+       x = "Species",
+       y = "Average Body Mass (g)"
+       ) + 
+  theme_classic()
+
+
+
+
 
 
 
