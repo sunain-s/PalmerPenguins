ad327d7e256a0f7d81e1f6bb8f5918e60230fc0a
sunain-s <Sunain.Syed@communities.gov.uk>
2026-06-24 18:08:33 +0100

    feat(penguins): interactive hover tooltip in browser with plotly

--- penguins.R
+++ penguins.R
@@ -1,8 +1,10 @@
 # install.packages("tidyverse")
 # install.packages("palmerpenguins")
+# install.packages("plotly")
 
 library(tidyverse)
 library(palmerpenguins)
+library(plotly)
 
 # head(penguins)
 # summary(penguins)
@@ -16,42 +18,79 @@ library(palmerpenguins)
 # Filter data
 avg_mass_by_species <- penguins %>%
   group_by(species) %>%
-  summarise(avg_mass = mean(body_mass_g, na.rm=TRUE),
+  summarise(n_penguins = sum(!is.na(body_mass_g)),
+            avg_mass = mean(body_mass_g, na.rm=TRUE),
             .groups = "drop") %>%
   arrange(desc(avg_mass)) %>%
   mutate(species = factor(species, levels = species))
-avg_mass_by_species
+print(avg_mass_by_species)
 
-# Plot data
-species_mass_plot <- ggplot(avg_mass_by_species,
-       aes(x = species, y = avg_mass, fill = species)) + 
+# Create static ggplot
+species_mass_plot <- ggplot(
+  avg_mass_by_species,
+  aes(
+    x = species,
+    y = avg_mass,
+    fill = species,
+    text = paste(
+      "Species:", species,
+      "<br>Average Mass:", round(avg_mass, 1), " g",
+      "<br>Penguins:", n_penguins
+    )
+  )
+) + 
   geom_col() +
-  labs(title = "Average Body Mass by Penguin Species",
-       x = "Species",
-       y = "Average Body Mass (g)"
-       ) + 
+  labs(
+    title = "Average Body Mass by Penguin Species",
+    x = "Species",
+    y = "Average Body Mass (g)"
+  ) +
   theme_classic() +
-  theme(legend.position = "none",
-        panel.grid.major.y = element_line(
-          colour = "grey85",
-          linewidth = 0.3
-        ),
-        panel.grid.minor.y = element_line(
-          colour = "grey92",
-          linewidth = 0.2
-        ))
+  theme(
+    legend.position = "none",
+    panel.grid.major.y = element_line(
+      colour = "grey85",
+      linewidth = 0.3
+      ),
+    panel.grid.minor.y = element_line(
+      colour = "grey92",
+      linewidth = 0.2
+      )
+    )
 species_mass_plot
 
-# Save plot image
+# Interactive version
+species_mass_plot_interactive <- ggplotly(
+  species_mass_plot,
+  tooltip = "text"
+)
+species_mass_plot_interactive
+
+
+# Save static plot image
 if (!dir.exists("output")) {
   dir.create("output")
 }
+if (!dir.exists("output/static")) {
+  dir.create("output/static")
+}
+if (!dir.exists("output/interactive")) {
+  dir.create("output/interactive")
+}
+
 ggsave(
-  "output/species_mass_plot.png",
+  "output/static/species_mass_plot.png",
   plot = species_mass_plot,
   width = 8,
   height = 5
 )
+
+htmlwidgets::saveWidget(
+  species_mass_plot_interactive,
+  "output/interactive/species_mass_plot.html",
+  selfcontained = TRUE
+)
+browseURL("output/interactive/species_mass_plot.html")
 
 
 
